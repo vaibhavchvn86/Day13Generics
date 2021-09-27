d7aa50e5e46896a3394cf5c53e1b07044b4ab572
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-27 21:53:09 +0530

    Find Max String

---
+++ UC3_Generics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13GenericsAssg
+{
+    class UC3_Generics
+    {
+        public static string MaxStringNum(string Num1, string Num2, string Num3)
+        {
+            if ((Num1).CompareTo(Num2) > 0 && (Num1).CompareTo(Num3) > 0||
+                (Num1).CompareTo(Num2) >= 0 && (Num1).CompareTo(Num3) > 0||
+                (Num1).CompareTo(Num2) > 0 && (Num1).CompareTo(Num3) >= 0)
+            {
+                return Num1;
+            }
+            if (Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) > 0||
+                Num2.CompareTo(Num1) >= 0 && Num2.CompareTo(Num3) > 0 ||
+                Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) >= 0 )
+            {
+                return Num2;
+            }
+            if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0||
+                Num3.CompareTo(Num1) >= 0 && Num3.CompareTo(Num2) > 0||
+                Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) >= 0)
+            {
+                return Num3;
+            }
+            throw new Exception("Num1, Num2, Num3 are same");
+        }
+    }
+}
