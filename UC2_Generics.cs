4b27ca2930fbed55ca9d7e9fb7633b0409a96fdc
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-27 21:51:51 +0530

    Find Max Float Number

---
+++ UC2_Generics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13GenericsAssg
+{
+    class UC2_Generics
+    {
+        public static double MaxFloatNum(double Num1, double Num2, double Num3)
+        {
+            if ((Num1).CompareTo(Num2) > 0 && (Num1).CompareTo(Num3) > 0)
+            {
+                return Num1;
+            }
+            if (Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) > 0)
+            {
+                return Num2;
+            }
+            if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0)
+            {
+                return Num3;
+            }
+            throw new Exception("Num1, Num2, Num3 are same");
+        }
+    }
+}
