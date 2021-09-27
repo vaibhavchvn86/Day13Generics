b7d33a6b31549e3a97f436964ea1af93d6cbb9da
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-27 21:51:25 +0530

    Find Max Integer Number

---
+++ UC1_Generics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13GenericsAssg
+{
+    class UC1_Generics
+    {
+        public static int MaxIntNum(int Num1, int Num2, int Num3)
+        {
+            if ((Num1).CompareTo(Num2) > 0 && (Num1).CompareTo(Num3) > 0)
+            {
+                return Num1;
+            }
+            if (Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) > 0)
+            {
+                return Num2;
+            }
+            if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0)
+            {
+                return Num3;
+            }
+            throw new Exception("Num1, Num2, Num3 are same");
+        }
+    }
+}
